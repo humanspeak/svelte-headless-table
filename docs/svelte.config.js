af1ad4daf1cdfd2f5c455989dcd240827734c94b
Jason Kummerl <jason.h@kummerl.com>
2025-03-07 15:12:58 -0500

    Fix things up

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,33 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+            mode: 'hash',
+            directives: {
+                'default-src': ['self'],
+                'script-src': [
+                    'self',
+                    'https://kit.fontawesome.com',
+                    'https://*.ingest.us.sentry.io',
+                    'unsafe-inline'
+                ],
+                'style-src': ['self', 'unsafe-inline', 'https://kit.fontawesome.com'],
+                'img-src': ['self', 'data:', 'https:'],
+                'font-src': [
+                    'self',
+                    'data:',
+                    'https://kit.fontawesome.com',
+                    'https://ka-p.fontawesome.com'
+                ],
+                'worker-src': ['self', 'blob:'],
+                'connect-src': ['self', 'https:'],
+                'frame-ancestors': ['none'],
+                'form-action': ['self'],
+                'base-uri': ['self'],
+                'upgrade-insecure-requests': true
+            }
+        }
 	},
 
 	extensions: ['.svelte', '.svx']
